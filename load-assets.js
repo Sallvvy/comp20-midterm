f0d22ba2ddba5ae21440ad362607c41cc40acf9f
Abe Park <abepark26@gmail.com>
2021-03-11 15:58:58 +0900

    src path fixing for index.html

--- load-assets.js
+++ load-assets.js
@@ -1,5 +1,5 @@
-$(function() {
+$("#main-container").ready($(function() {
     $("#header").load("/assets/header.html");
     $("#navigation").load("/assets/navigation.html");
     $("footer").load("/assets/footer.html");
-})
+}))
