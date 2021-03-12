f392ab474c33eeacf0af87e2da728cfdca6024d8
Abe Park <abepark26@gmail.com>
2021-03-12 11:23:34 +0900

    changed id names for nav bar

--- load-assets.js
+++ load-assets.js
@@ -1,5 +1,5 @@
 $("#main-container").ready($(function() {
-    $("#header").load("/comp20-midterm/assets/header.html");
-    $("#navigation").load("/comp20-midterm/assets/navigation.html");
-    $("footer").load("/comp20-midterm/assets/footer.html");
+    $("#header-container").load("/comp20-midterm/assets/header.html");
+    $("#navigation-container").load("/comp20-midterm/assets/navigation.html");
+    $("footer-container").load("/comp20-midterm/assets/footer.html");
 }))
