fca17d4db3e82ea0527df83fb25bad96d59d77ab
Abe Park <abepark26@gmail.com>
2021-03-13 18:22:23 +0900

    nav bar debugging

--- load-assets.js
+++ load-assets.js
@@ -1,5 +1,5 @@
-window.addEventListener("load", $(function() {
+$(function() {
     $("#header-container").load("/comp20-midterm/assets/header.html");
     $("#navigation-container").load("/comp20-midterm/assets/navigation.html");
     $("#footer-container").load("/comp20-midterm/assets/footer.html");
-}))
+})
