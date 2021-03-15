24fb906f2bbeb85784a78e5fd338a507937ff0e7
Abe Park <abepark26@gmail.com>
2021-03-15 10:56:36 +0900

    home page edits

--- load-assets.js
+++ load-assets.js
@@ -1,5 +1,5 @@
-$(function() {
+window.onload=function() {
     $("#header-container").load("/comp20-midterm/assets/header.html");
     $("#navigation-container").load("/comp20-midterm/assets/navigation.html");
     $("#footer-container").load("/comp20-midterm/assets/footer.html");
-})
+}
